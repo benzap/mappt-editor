d5550fa036a6aa35ef952c45028fc0f3166d9757
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-04-13 17:35:37 -0400

    Removing rest of JS

--- js/mappt/mappt-config.js
+++
@@ -1,9 +0,0 @@
-/*
-  includes a bunch of configuration stuff. Particularly for things
-  that might change if the code were used on a different site.
-*/
-
-//Directory partial paths
-//partial path for the maps
-var Mappt_Layout_Folder = "./floorPlans_svg/";
-var Mappt_Data_Folder = "./floorPlans_data/";
