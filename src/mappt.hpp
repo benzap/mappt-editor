05ff0bf000befd5ebbddb67cf421ce40f50a3b50
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-04-22 20:07:27 -0400

    Added a file

---
+++ src/mappt.hpp
@@ -0,0 +1,24 @@
+#ifndef MAPPT__HPP
+#define MAPPT__HPP
+//DESCRIPTION
+/*
+  This is the entry-point into the mappt library
+ */
+
+//CLASSES
+class Mappt;
+
+//INCLUDES
+
+//DEFINITIONS
+
+//MACROS
+
+//TYPEDEFS
+
+//FUNCTIONS
+
+//BEGIN
+
+
+#endif //END MAPPT__HPP
