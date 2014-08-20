2841a639c0f0af503ab53b29d54f88d67e78c459
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2014-08-19 22:22:56 -0400

    Finished setting up for clojure+clojurescript

--- project.clj
+++ project.clj
@@ -1,6 +1,19 @@
 (defproject mappt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Map Application to get from point A to point B"
+  :source-paths ["src-clj"]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2197"
+                  :exclusions [org.apache.ant/ant]]
+                 [compojure "1.1.8"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.11"]]
+  :cljsbuild {
+              :builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
+  :ring {:handler mappt.server.routes/app})
--- src/mappt/core.clj
+++
@@ -1,6 +0,0 @@
-(ns mappt.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
