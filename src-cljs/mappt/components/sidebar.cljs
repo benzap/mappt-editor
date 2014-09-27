453d52a1d09dd8f7d23175dab0b9c05c16037f5a
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-09-27 18:43:06 -0400

    properly working sidebar

--- src-cljs/mappt/components/sidebar.cljs
+++ src-cljs/mappt/components/sidebar.cljs
@@ -1,45 +1,43 @@
 (ns mappt.components.sidebar
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [cljs.core.async :refer [put! chan <!]]
-            [secretary.core :as secretary]
             [mappt.style.icon :refer [gen-icon]]))
 
-(def navigate-channel (chan))
-
-(defn click-handler [state url name e]
-  (.log js/console "clicked!" url)
-  (om/transact! state [:sidebar :selected] #(str name))
-  (put! navigate-channel url))
-
-(defn sidebar-widget [{:keys [name url icon] :as state} owner]
+(defn sidebar-widget [{:keys [name url icon] :as data} owner]
   (reify
     om/IRenderState
-    (render-state [this {:keys [selected] :as state}]
-      (html [:div {:class "sidebar-element"
+    (render-state [this {:keys [selected select-chan] :as state}]
+      (html [:div {:class
+                   (str "sidebar-element"
+                        (when (= name selected) " selected"))
                    :on-click
                    (fn [_]
-                     (.log js/console "clicked!" name selected))}
-             [:div {:class "sidebar-icon"}]
+                     (put! select-chan name)
+                     (om/refresh! owner))}
+             [:div {:class (str "sidebar-icon")}]
              [:div {:class "sidebar-name"} name]]))))
-
-(defn gen-sidebar-element [state & {:keys [name url icon selected]
-                                    :or {name "unknown"
-                                         url "/#/"
-                                         icon ""
-                                         selected false}}]
-  [:div {:class (str "sidebar-element" (when selected " selected"))
-         :on-click (partial click-handler state url name)}
-   [:div {:class "sidebar-icon"}]
-   [:div {:class "sidebar-name"} name]])
 
 (defn widget [data owner]
   (reify
-    om/IRender
-    (render [this]
+    om/IInitState
+    (init-state [this]
+      {:select-chan (chan)})
+    om/IWillMount
+    (will-mount [this]
+      (let [select-chan (om/get-state owner :select-chan)]
+        (go (loop []
+              (let [selected (<! select-chan)]
+                (om/update! data [:sidebar :selected] selected)
+                (recur))))))
+    om/IRenderState
+    (render-state [this {:keys [select-chan] :as state}]
       (html [:div {:class "sidebar-navigation"}
              (om/build-all
               sidebar-widget
               (get-in data [:sidebar :buttons])
               {:init-state
+               {:select-chan select-chan}
+               :state
                {:selected (get-in data [:sidebar :selected])}})]))))
